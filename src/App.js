d5dee6cb2ecd9887b1be01d254b889b9c1fa7953
Shakibshalim2 <mdshakibshalim@gmail.com>
2025-08-28 02:00:14 +0600

    done

--- src/App.js
+++ src/App.js
@@ -20,8 +20,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/" element={<AdminLogin />} />
         <Route element={<ProtectedRoute />}>
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
         </Route>
